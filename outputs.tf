4df0e7311121c23b12c70f5f944bd9e97a34500d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "kubernetes_fleet_members" {
-  description = "All kubernetes_fleet_member resources"
-  value       = azurerm_kubernetes_fleet_member.kubernetes_fleet_members
-}
 output "kubernetes_fleet_members_group" {
-  description = "List of group values across all kubernetes_fleet_members"
-  value       = [for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : v.group]
+  description = "Map of group values across all kubernetes_fleet_members, keyed the same as var.kubernetes_fleet_members"
+  value       = { for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : k => v.group }
 }
 output "kubernetes_fleet_members_kubernetes_cluster_id" {
-  description = "List of kubernetes_cluster_id values across all kubernetes_fleet_members"
-  value       = [for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : v.kubernetes_cluster_id]
+  description = "Map of kubernetes_cluster_id values across all kubernetes_fleet_members, keyed the same as var.kubernetes_fleet_members"
+  value       = { for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : k => v.kubernetes_cluster_id }
 }
 output "kubernetes_fleet_members_kubernetes_fleet_id" {
-  description = "List of kubernetes_fleet_id values across all kubernetes_fleet_members"
-  value       = [for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : v.kubernetes_fleet_id]
+  description = "Map of kubernetes_fleet_id values across all kubernetes_fleet_members, keyed the same as var.kubernetes_fleet_members"
+  value       = { for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : k => v.kubernetes_fleet_id }
 }
 output "kubernetes_fleet_members_name" {
-  description = "List of name values across all kubernetes_fleet_members"
-  value       = [for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : v.name]
+  description = "Map of name values across all kubernetes_fleet_members, keyed the same as var.kubernetes_fleet_members"
+  value       = { for k, v in azurerm_kubernetes_fleet_member.kubernetes_fleet_members : k => v.name }
 }
 
